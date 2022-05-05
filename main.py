bb8df1c7d2b021e9456a2f3ebd79e34f13c04163
Sean Yoon <92048016+seanyoon814@users.noreply.github.com>
2022-05-04 18:39:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from RiotAPI import RiotAPI
 
 def main():
-    api = RiotAPI('RGAPI-71b5db48-490e-42de-b196-49a5f7078b2e')
+    api = RiotAPI('')
     r = api.getSummonerByName('ff15open')
     print(r)
 if __name__ == "__main__":
