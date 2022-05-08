1ce5c7bffdede16b3a80c735c1833bf909a86d54
Sean Yoon <92048016+seanyoon814@users.noreply.github.com>
2022-05-07 20:39:13 -0700

    basic UI finished (!stats)

--- RiotAPI.py
+++ RiotAPI.py
@@ -1,4 +1,6 @@
 from weakref import proxy
+
+from attr import define
 import RiotConsts as consts
 import requests
 class RiotAPI(object):
@@ -13,7 +15,15 @@ class RiotAPI(object):
         response = requests.get(consts.URL['base'].format(url=api_url), params=args)
         print(response.url)
         return response.json()
-    
+
+    def requestKDA(self, api_url, params={}):
+        args = {'api_key': self.api_key}
+        for key, value in params.items():
+            if key not in args:
+                args[key] = value
+        response = requests.get(consts.URL['base2'].format(url=api_url), params=args)
+        print(response.url)
+        return response.json()
     def getSummonerByName(self, name):
         api_url = consts.URL['summonerByName'].format(
             names=name
@@ -27,6 +37,13 @@ class RiotAPI(object):
             return response['id']
         except: # response['status']['status_code'] == 404:
             return None
+    def getSummonerPID(self, name):
+        response = self.getSummonerByName(name)
+        try:
+            print(response['puuid'])
+            return response['puuid']
+        except: # response['status']['status_code'] == 404:
+            return None 
     # tier = Gold, rank = I, leaguePoints = 51, wins = 56, losses = 50
     def getSummonerRank(self, name):
         response = self.getSummonerRankInfo(name)
@@ -34,18 +51,72 @@ class RiotAPI(object):
             return response[0]
         except:
             return None
-    
+    def getTopChamps(self, name):
+        response = self.getTopChampsHelper(name)
+        responseTop5 = []
+        try:
+            for i in range(5):
+                champ = str(response[i]['championId'])
+                mpt = str(response[i]['championPoints'])
+                responseTop5.append(consts.CHAMP_ID[champ])
+                responseTop5.append(mpt)
+            return responseTop5
+        except:
+            return None
+    # O(N^2), maybe improve?
+    # def getKDA(self, name):
+    #     kda = 0 # [[kills], [deaths], [assists]]
+    #     index = 0
+    #     pid = self.getSummonerPID(name)
+    #     while index < 5:
+    #         gameNum = 0
+    #         gameID = self.getGameID(name)[index]
+    #         game = self.getGame(gameID)
+    #         while pid != game['metadata']['participants'][gameNum]:
+    #             gameNum+=1
+    #         kda += game['info']['participants'][gameNum]['challenges']['kda']
+    #         index+=1
+    #     return kda
     # Helper Functions
     def getSummonerByName(self, name):
         api_url = consts.URL['summonerByName'].format(
             names=name
         )
         return self.request(api_url)
-    
     def getSummonerRankInfo(self, name):
         iden = self.getSummonerID(name)
         api_url = consts.URL['rankStats'].format(
             id = iden
         )
         return self.request(api_url)
+
+    def getTopChampsHelper(self, name):
+        iden = self.getSummonerID(name)
+        api_url = consts.URL['topChamps'].format(
+            id = iden
+        )
+        return self.request(api_url)
+
+    def getRankImg(self, name):
+        return consts.IMAGES[name]
+    
+    def getGameID(self, name):
+        puid = self.getSummonerPID(name)
+        api_url = consts.URL['kda'].format(
+            pid = puid 
+        )
+        return self.requestKDA(api_url)
+
+    def getGame(self, gameID):
+        api_url = consts.URL['kdaCalc'].format(
+            gameIden = gameID
+        )
+        return self.requestKDA(api_url)
+
+    def formatChamps(self, arr):
+        str = ""
+        for i in range(0, 10, 2):
+            str+= arr[i] + ' (' + arr[i+1] + ' Mastery Points)\n'
+        return str
+
 
--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='stats', help='Displays summoner statistics') 
 async def getStats(ctx, summonerName):
-    api = RiotAPI('RGAPI-ac6cc217-9f3c-40be-a01b-a6594ace1fc2')
+    api = RiotAPI('RGAPI-0772719d-e9eb-42c1-9f7e-6384bfcc282b')
     stats = api.getSummonerRank(summonerName)
     if(stats == None):
         await ctx.send('The name does not exist. Summoner names must exist in NA servers.')
@@ -22,12 +22,27 @@ async def getStats(ctx, summonerName):
         wins = stats['wins']
         losses = stats['losses']
         ratio = int(wins/(wins+losses)*100)
-        await ctx.send('Summoner Name: ' + stats['summonerName']
-        +'\nRank: ' + stats['tier'].capitalize() + ' ' + stats['rank'] + ' ' + str(stats['leaguePoints']) + ' LP'
-        + '\n' + str(ratio) + '% WR' + ' (' + str(wins) + 'W ' + str(losses) + 'L)')
+        top5 = api.getTopChamps(summonerName)
+        
+        #ranked players
+        # await ctx.send('Summoner Name: ' + stats['summonerName']
+        # +'\nRank: ' + stats['tier'].capitalize() + ' ' + stats['rank'] + ' ' + str(stats['leaguePoints']) + ' LP'
+        # + '\n' + str(ratio) + '% WR' + ' (' + str(wins) + 'W ' + str(losses) + 'L)'
+        # + top5[0] + "\n" + top5[1] + "\n" + top5[2] + "\n" + top5[3] + "\n" + top5[4] + "\n", 
+        # file=discord.File(api.getRankImg(stats['tier'])))
+        embed=discord.Embed(title="League of Legends Ranked (Solo/Duo) Statistics for " + stats['summonerName'], color=0x00ff9d)
+        file = discord.File(str(api.getRankImg(stats['tier'])), filename="image.png")
+        embed.set_thumbnail(url="attachment://image.png")
+        embed.add_field(name="Rank", value=stats['tier'].capitalize() + ' ' + stats['rank'] + ' ' + str(stats['leaguePoints']) + ' LP', inline=True)
+        embed.add_field(name="Statistics", value= str(wins) + 'W ' + str(losses) + 'L ' + '(' + str(ratio) + '% WR)', inline=True)
+        # + str(api.getKDA(summonerName)/5.0)
+        embed.add_field(name="Most Played Champions", value="" + api.formatChamps(top5), inline=False)
+        await ctx.send(file = file, embed=embed)
+
 
 @bot.command(name='live', help='Live game statistics') 
 async def getLive(ctx, summonerName):
     api = RiotAPI('RGAPI-ac6cc217-9f3c-40be-a01b-a6594ace1fc2')
-    
+
+
 bot.run(TOKEN)
